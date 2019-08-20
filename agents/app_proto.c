8442aaffe32a71dde934d482339f293a75f23a8b
Marios Kogias <marios.kogias@epfl.ch>
2019-08-20 17:06:29 +0200

    Implement entries with no side-effects

--- agents/app_proto.c
+++ agents/app_proto.c
@@ -112,8 +112,22 @@ int synthetic_create_request(struct application_protocol *proto,
 int rep_synth_create_request(struct application_protocol *proto,
 							 struct request *req)
 {
-	synthetic_create_request(proto, req);
-	req->meta = (void *)(unsigned long)2; // replicated route
+	long * rep_arg = (long *)proto->arg;
+	struct rand_gen *generator = (struct rand_gen *)rep_arg[0];
+
+	if (!per_thread_arg) {
+		per_thread_arg = malloc(sizeof(long));
+		assert(per_thread_arg);
+	}
+	per_thread_arg = (void *)lround(generate(generator));
+	req->iovs[0].iov_base = &per_thread_arg;
+	req->iovs[0].iov_len = sizeof(long);
+	req->iov_cnt = 1;
+
+	if (drand48()*100 <= rep_arg[1])
+		req->meta = (void *)(unsigned long)3; // replicated route no side effects
+	else
+		req->meta = (void *)(unsigned long)2; // replicated route no side effects
 
 	return 0;
 }
@@ -134,6 +148,7 @@ static int synthetic_init(char *proto, struct application_protocol *app_proto)
 {
 	char *token;
 	struct rand_gen *gen = NULL;
+	long *rep_arg;
 
 	// Remove the type.
 	token = strtok(proto, ":");
@@ -142,13 +157,21 @@ static int synthetic_init(char *proto, struct application_protocol *app_proto)
 	gen = init_rand(token);
 	assert(gen);
 
-	app_proto->type = PROTO_SYNTHETIC;
-	// The proto arg is the random generator
-	app_proto->arg = gen;
-	if (strncmp(proto, "rep", 3) == 0)
+	if (strncmp(proto, "rep", 3) == 0) {
+		rep_arg = malloc(2*sizeof(long));
+		assert(rep_arg);
+		rep_arg[0] = (long) gen;
+		token = strtok(NULL, "");
+		rep_arg[1] = atof(token);
+		printf("Percentage of NO_SIDE_EFFECT is %ld\n", rep_arg[1]);
+		app_proto->arg = rep_arg;
+		app_proto->type = PROTO_REP_SYNTHETIC;
 		app_proto->create_request = rep_synth_create_request;
-	else
+	} else {
+		app_proto->type = PROTO_SYNTHETIC;
+		app_proto->arg = gen;
 		app_proto->create_request = synthetic_create_request;
+	}
 	app_proto->consume_response = synthetic_consume_response;
 
 	return 0;
